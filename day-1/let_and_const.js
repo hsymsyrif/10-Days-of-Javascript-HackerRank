dc703466791e90d20dc01be3105c9729eeecbd2a
hsymsyrif <hisyams191@gmail.com>
2025-03-13 10:33:44 +0700

    day1

---
+++ day-1/let_and_const.js
@@ -0,0 +1,49 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  // Declare a constant variable PI and assign it the value of Math.PI
+  const PI = Math.PI;
+
+  // Read input radius from stdin
+  const r = parseFloat(readLine());
+
+  // Calculate and print the area of the circle
+  console.log(PI * r * r);
+
+  // Calculate and print the perimeter (circumference) of the circle
+  console.log(2 * PI * r);
+
+  try {
+    // Attempt to redefine the value of constant variable PI
+    PI = 0;
+    // Attempt to print the value of PI
+    console.log(PI);
+  } catch (error) {
+    console.error("You correctly declared 'PI' as a constant.");
+  }
+}
